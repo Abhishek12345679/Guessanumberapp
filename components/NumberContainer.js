f5446a40bc228a0091fb720a8f2a7fe1b822314c
Abhishek sah <sah755146@gmail.com>
2020-02-19 10:23:41 +0530

    test

--- components/NumberContainer.js
+++ components/NumberContainer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import {Button,fText, StyleSheet } from "react-native";
 
 import Circle from '../components/CircleShape'
 
